4c20a67cf498e97703473ee97db6d9ec27a9efa2
Alex Samylkin <134559+mortid0@users.noreply.github.com>
2023-07-05 14:08:02 -0700

    Fix feature dimensions

--- client.py
+++ client.py
@@ -12,7 +12,7 @@ class Load(threading.Thread):
             nbs_1hop  = graph.sample_neighbors(nodes=node_ids, edge_types=np.array([0], np.int32), count=10)[0]
             nbs_2hop  = graph.sample_neighbors(nodes=nbs_1hop.flatten(), edge_types=np.array([0], np.int32), count=10)[0]
             all_nodes = np.concatenate((node_ids, nbs_1hop.flatten(), nbs_2hop.flatten()))
-            f = graph.node_features(nodes=all_nodes, features=np.array([[0, 300], [1, 50]], dtype=np.int32), feature_type=np.float32)
+            f = graph.node_features(nodes=all_nodes, features=np.array([[0, 50], [1, 300]], dtype=np.int32), feature_type=np.float32)
             print(f"Got features: {f.sum()}")
             assert f.sum() > 0
             time.sleep(0.1)
