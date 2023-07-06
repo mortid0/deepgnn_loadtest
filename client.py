89992589a331a2b18a24ff7cfc51384cad2358df
Alex Samylkin <134559+mortid0@users.noreply.github.com>
2023-07-05 17:39:17 -0700

    Format

--- client.py
+++ client.py
@@ -2,20 +2,41 @@ import threading
 import time
 import numpy as np
 import deepgnn.graph_engine.snark.distributed as distributed
+
+
 class Load(threading.Thread):
     def run(self, *args, **kwargs):
-        servers = ["10.1.0.4:50051", "10.1.0.4:50052", "10.1.0.4:50053", "10.1.0.4:50054", "10.1.0.4:50055", "10.1.0.4:50056"]
+        servers = [
+            "10.1.0.4:50051",
+            "10.1.0.4:50052",
+            "10.1.0.4:50053",
+            "10.1.0.4:50054",
+            "10.1.0.4:50055",
+            "10.1.0.4:50056",
+        ]
         graph = distributed.Client(servers)
         print(f"Created client, connected to servers")
         while True:
-            node_ids = np.random.randint(0, 152410, 1000, dtype=np.int64)
-            nbs_1hop  = graph.sample_neighbors(nodes=node_ids, edge_types=np.array([0], np.int32), count=10)[0]
-            nbs_2hop  = graph.sample_neighbors(nodes=nbs_1hop.flatten(), edge_types=np.array([0], np.int32), count=10)[0]
-            all_nodes = np.concatenate((node_ids, nbs_1hop.flatten(), nbs_2hop.flatten()))
-            f = graph.node_features(nodes=all_nodes, features=np.array([[0, 50], [1, 300]], dtype=np.int32), feature_type=np.float32)
+            node_ids = np.random.randint(0, 231443, 1000, dtype=np.int64)
+            nbs_1hop = graph.sample_neighbors(
+                nodes=node_ids, edge_types=np.array([0, 1, 2], np.int32), count=10
+            )[0]
+            nbs_2hop = graph.sample_neighbors(
+                nodes=nbs_1hop.flatten(),
+                edge_types=np.array([0, 1, 2], np.int32),
+                count=10,
+            )[0]
+            all_nodes = np.concatenate(
+                (node_ids, nbs_1hop.flatten(), nbs_2hop.flatten())
+            )
+            f = graph.node_features(
+                nodes=all_nodes,
+                features=np.array([[0, 50], [1, 300]], dtype=np.int32),
+                feature_type=np.float32,
+            )
             print(f"Got features: {f.sum()}")
-            assert f.sum() > 0
-            time.sleep(0.1)
+            time.sleep(0.01)
+
 
 l = Load()
 l.start()
--- server.py
+++ server.py
@@ -6,12 +6,15 @@ import os
 def download_data(dir: str, num_partitions: int = 6):
     if os.path.exists(dir):
         print("Data already exists. Skipping download.")
-        return 
+        return
     # use a separate function to unload Reddit graph from temp memory
     os.makedirs(dir, exist_ok=True)
     r = reddit.Reddit(output_dir=dir, num_partitions=num_partitions)
 
-def start_servers(dir: str, addresses: list[str] = None, partitions: list[list[int]] = None):
+
+def start_servers(
+    dir: str, addresses: list[str] = None, partitions: list[list[int]] = None
+):
     # start the servers
     assert len(addresses) == len(partitions)
     servers = []
@@ -20,13 +23,19 @@ def start_servers(dir: str, addresses: list[str] = None, partitions: list[list[i
         print(f"Started server {i} at {addresses[i]}")
     return servers
 
+
 data_dir = "/tmp/reddit"
-addresses = ["10.1.0.4:50051", "10.1.0.4:50052", "10.1.0.4:50053", "10.1.0.4:50054", "10.1.0.4:50055", "10.1.0.4:50056"]
+addresses = [
+    "10.1.0.4:50051",
+    "10.1.0.4:50052",
+    "10.1.0.4:50053",
+    "10.1.0.4:50054",
+    "10.1.0.4:50055",
+    "10.1.0.4:50056",
+]
 partitions = [[0], [1], [2], [3], [4], [5]]
 print("Downloading data...")
 download_data(data_dir, len(addresses))
 print("Starting servers...")
 s = start_servers(data_dir, addresses, partitions)
 input("Serving requests. Press Enter to stop and exit...")
-
-
